11b88e5355ac81d1b905e7e268dd337c3ed9b502
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2024-02-22 17:20:09 +0100

    Update main.java

--- main.java
+++ main.java
@@ -3,6 +3,8 @@ import java.net.URL;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main {
     public static void main(String[] args) throws IOException {
