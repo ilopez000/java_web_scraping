6286ac103f287a20f846e823370e35dc47b8e4dd
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2024-02-28 16:54:22 +0100

    Create ejemplo_web_prateducacio.java

---
+++ ejemplo_web_prateducacio.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://www.prateducacio.com/lescola-prat-educacio/");
+
+        // Obtener el HTML directamente con Jsoup
+        Document document = Jsoup.connect(url.toString()).get();
+        //getElementByID
+
+        // Seleccionar los tags
+        Elements headers = document.select("h4");
+
+        for(int i=0; i<headers.size(); i++){
+            System.out.println(headers.get(i));
+        }
+
+        System.out.println(headers);
+
+
+    }
+}
